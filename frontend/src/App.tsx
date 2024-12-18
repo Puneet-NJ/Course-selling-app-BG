001ab8a792c8cd3cc57169b966bed6d9ad71b2ae
Puneet NJ <lpuneetnj@gmail.com>
2024-12-19 01:34:57 +0530

    added navbar, sidebar & footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserHome from "./Pages/UserHome";
+import UserLayout from "./UserLayout";
+
 function App() {
-	return <div></div>;
+	return (
+		<main>
+			<BrowserRouter>
+				<UserLayout>
+					<Routes>
+						<Route path="/" element={<UserHome />} />
+					</Routes>
+				</UserLayout>
+			</BrowserRouter>
+		</main>
+	);
 }
 
 export default App;
---
+++ frontend/src/utils/Logo.tsx
@@ -0,0 +1,7 @@
+import { LOGO_URL } from "./lib";
+
+const Logo = () => {
+	return <img src={LOGO_URL} alt="logo" />;
+};
+
+export default Logo;
